4d642201d660a51373d7db969a7ad88a5acf7675
ByteCoder <rreyes@canaryhealth.com>
2020-04-14 12:48:24 -0500

    First set of features added

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# RoyalLibrary
+# Royal Library
+
 Some extensions methods like the ones in Ruby .each .map and more
+
+## Examples
+
+```
+var myArray = new[] { 1, 45, 34, 435 };
+
+// Evens and odds
+myArray.Evens();
+myArray.Odds();
+
+// Sum
+myArray.TotalAllEvens();
+myArray.TotalAllOdds();
+
+// Each
+myArray.Each(item => Debug.Log($"Using the strength: {item}"));
+
+// Map
+myArray.Map(item => item * 2).Each(item => Debug.Log($"Using the strength doubled: {item}"));
+
+// Times
+5.Times(_ => {
+  var theVar = 13;
+  Debug.Log($"This is {theVar}");
+});
+```
---
+++ RoyalLibrary/RoyalExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoyalLibrary {
+  public static class RoyalExtensions {
+    public static void Times(this int source, Action<int> action) {
+      for(var i = 0; i < source; i++) {
+        action(i);
+      }
+    }
+
+    #region Count Utilities
+    public static IEnumerable<int> Evens(this IEnumerable<int> numbers) => numbers.Where(n => n % 2 == 0);
+
+    public static IEnumerable<int> Odds(this IEnumerable<int> numbers) => numbers.Where(n => n % 2 != 0);
+
+    public static long TotalAllEvens(this IEnumerable<int> numbers) => numbers.Evens().Sum();
+
+    public static long TotalAllOdds(this IEnumerable<int> numbers) => numbers.Odds().Sum();
+    #endregion 
+
+    #region Enumerables
+    public static void Each<T>(this IEnumerable<T> source, Action<T> action) {
+      foreach(var element in source) {
+        action(element);
+      }
+    }
+
+    public static IEnumerable<TU> Map<T, TU>(this IEnumerable<T> source, Func<T, TU> action) {
+      var enumerable = new List<TU>();
+      foreach(var element in source) {
+        enumerable.Add(action(element));
+      }
+
+      return enumerable;
+    }
+    #endregion
+  }
+}
