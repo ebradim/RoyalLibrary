e71f38a75f4ec6f05187c942b3ad36ca6adff4e8
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-04-21 14:22:31 -0500

    xUnit Times test added

--- RoyalLibrary.Tests/RoyalExtensionsTests.cs
+++ RoyalLibrary.Tests/RoyalExtensionsTests.cs
@@ -3,19 +3,19 @@ using Xunit;
 
 namespace RoyalLibrary.Tests
 {
-    public class RoyalExtensionsTests
+  public class RoyalExtensionsTests
+  {
+    [Fact]
+    public void TimesExecutesFiveTimes()
     {
-        [Fact]
-        public void TimesExecutesFiveTimes()
-        {
-            // Arrange
-            var count = 0;
-            
-            // Act
-            5.Times { _ => count++ }    
+      // Arrange
+      var count = 0;
 
-            // Assert
-            Assert.Equal(5, count);
-        }
+      // Act
+      5.Times(_ => count++);
+
+      // Assert
+      Assert.Equal(5, count);
     }
+  }
 }
