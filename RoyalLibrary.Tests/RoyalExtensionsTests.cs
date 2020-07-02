efda199d3c6439680e240f59978665d2ca4352b1
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-07-01 20:06:18 -0500

    Changed root namespace to ByteDecoder.RoyalLibrary

--- RoyalLibrary.Tests/RoyalExtensionsTests.cs
+++ RoyalLibrary.Tests/RoyalExtensionsTests.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using ByteDecoder.RoyalLibrary;
 using RoyalLibrary.Tests.Fakes;
 using Xunit;
 
--- RoyalLibrary/ConditionalIndexExtensions.cs
+++ RoyalLibrary/ConditionalIndexExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace RoyalLibrary
+namespace ByteDecoder.RoyalLibrary
 {
   /// <summary>
   /// 
