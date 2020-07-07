e3655a29ffc9e75e70e85494c56e2378fe438f79
ByteCoder <rreyes@canaryhealth.com>
2020-07-07 01:07:49 -0500

    Added TopIndexes_ThrowsAnArgumentNullException_WhenPredicateDelegateIsNull xUnit test

--- src/RoyalLibrary.Tests/ConditionalIndexExtensionsTests.cs
+++ src/RoyalLibrary.Tests/ConditionalIndexExtensionsTests.cs
@@ -1,7 +1,32 @@
+using ByteDecoder.RoyalLibrary;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
 namespace RoyalLibrary.Tests
 {
-    public class ConditionalIndexExtensionsTests
+  public class ConditionalIndexExtensionsTests
+  {
+    [Fact]
+    public void TopIndexes_ThrowsAnArgumentNullException_WhenSourceIsNull()
     {
-        
+      // Arrange
+      List<bool> source = null;
+
+      // Act
+      // Assert
+      Assert.Throws<ArgumentNullException>(() => source.TopIndexes(element => element, 4));
     }
+
+    [Fact]
+    public void TopIndexes_ThrowsAnArgumentNullException_WhenPredicateDelegateIsNull()
+    {
+      // Arrange
+      var source = new bool[] { true, true, false };
+
+      // Act
+      // Assert
+      Assert.Throws<ArgumentNullException>(() => source.TopIndexes(null, 4));
+    }
+  }
 }
