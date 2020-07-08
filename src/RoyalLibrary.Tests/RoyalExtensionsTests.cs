32af383507269dd636b83eb2eccd488f521dfa9a
ByteCoder <rreyes@canaryhealth.com>
2020-07-08 02:40:12 -0500

    Moved Times logic into its own class TimeExtensions

--- src/RoyalLibrary.Tests/RoyalExtensionsTests.cs
+++ src/RoyalLibrary.Tests/RoyalExtensionsTests.cs
@@ -17,51 +17,6 @@ namespace RoyalLibrary.Tests
     }
 
     [Fact]
-    public void TimesParams_ExecutesProperly_WhenIsNeededFiveTimes()
-    {
-      // Arrange
-      var count = 0;
-
-      // Act
-      5.Times(_ => count++);
-
-      // Assert
-      Assert.Equal(5, count);
-    }
-
-    [Fact]
-    public void TimesNoParams_ExecutesProperly_WhenIsNeededFiveTimes()
-    {
-      // Arrange
-      var count = 0;
-
-      // Act
-      5.Times(() => count++);
-
-      // Assert
-      Assert.Equal(5, count);
-    }
-
-    [Fact]
-    public void TimesNoParams_ThrowsAnArgumentNullException_WhenActionDelegateIsNull()
-    {
-      // Arrange
-      // Act
-      // Assert
-      Assert.Throws<ArgumentNullException>(() => 5.Times((Action) null));
-    }
-
-
-    [Fact]
-    public void TimesParams_ThrowsAnArgumentNullException_WhenActionDelegateIsNull()
-    {
-      // Arrange
-      // Act
-      // Assert
-      Assert.Throws<ArgumentNullException>(() => 5.Times((Action<int>) null));
-    }
-
-    [Fact]
     public void Evens_ReturnsValidOutput_WhenHasValidSource()
     {
       // Arrange 
---
+++ src/RoyalLibrary.Tests/TimeExtensionsTests.cs
@@ -0,0 +1,55 @@
+using ByteDecoder.RoyalLibrary;
+using System;
+using Xunit;
+
+namespace RoyalLibrary.Tests
+{
+  public class TimeExtensionsTests
+  {
+
+    [Fact]
+    public void TimesParams_ExecutesProperly_WhenIsNeededFiveTimes()
+    {
+      // Arrange
+      var count = 0;
+
+      // Act
+      5.Times(_ => count++);
+
+      // Assert
+      Assert.Equal(5, count);
+    }
+
+    [Fact]
+    public void TimesNoParams_ExecutesProperly_WhenIsNeededFiveTimes()
+    {
+      // Arrange
+      var count = 0;
+
+      // Act
+      5.Times(() => count++);
+
+      // Assert
+      Assert.Equal(5, count);
+    }
+
+    [Fact]
+    public void TimesNoParams_ThrowsAnArgumentNullException_WhenActionDelegateIsNull()
+    {
+      // Arrange
+      // Act
+      // Assert
+      Assert.Throws<ArgumentNullException>(() => 5.Times((Action)null));
+    }
+
+
+    [Fact]
+    public void TimesParams_ThrowsAnArgumentNullException_WhenActionDelegateIsNull()
+    {
+      // Arrange
+      // Act
+      // Assert
+      Assert.Throws<ArgumentNullException>(() => 5.Times((Action<int>)null));
+    }
+  }
+}
