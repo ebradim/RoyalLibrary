a04ffc0815d1714fd9d239b7950fd275116e08a9
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-07-09 22:56:01 -0500

    Addeed IsGreaterThan_ReturnsFalse_WhenIsNotGreater xUnit test

--- src/RoyalLibrary.Tests/MathComparisonExtensionsTests.cs
+++ src/RoyalLibrary.Tests/MathComparisonExtensionsTests.cs
@@ -5,7 +5,20 @@ namespace RoyalLibrary.Tests
   public class MathComparisonExtensionsTests
   {
     [Fact]
-    public void IsGreaterThan_ReturnsValidResult_WhenHaveValidInput()
+    public void IsGreaterThan_ReturnsTrue_WhenIsGreater()
+    {
+      // Arrange
+      var number = 123;
+
+      // Act
+      var result = number.IsGreaterThan(12);
+
+      // Assert
+      Assert.True(result);
+    }
+
+    [Fact]
+    public void IsGreaterThan_ReturnsFalse_WhenIsNotGreater()
     {
       // Arrange
       var number = 123;
